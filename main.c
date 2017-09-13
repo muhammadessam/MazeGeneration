c398c5a7c965e21c05381ab8972abba6f265d428
muhammad <mohammedessam699@gmail.com>
2017-09-13 20:23:22 +0200

    at the beginning

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <spl/cslib.h>
+#include <spl/gwindow.h>
+#include <spl/gevents.h>
+#include <spl/gobjects.h>
+
+#define WIDTH 400
+#define HEIGHT 400
+#define CELLWIDTH 20
+#define rows HEIGHT/CELLWIDTH
+#define cols WIDTH/CELLWIDTH
+
+
+
+
+
+
+
+int main() {
+    GWindow panel;
+    panel = newGWindow(WIDTH,HEIGHT);
+    
+}
